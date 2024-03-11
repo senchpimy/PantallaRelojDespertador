ead4d87979c6d6bd00d7e0dc2b77f347c2a91239
senchpimy <senchpimy@gmail.com>
2024-03-10 23:39:58 -0600

    first commit

---
+++ src/Calendar.svelte
@@ -0,0 +1,58 @@
+<script>
+  import { onMount } from "svelte";
+  import { Calendar } from "fullcalendar";
+
+  function cal() {
+    const calendarEl = document.getElementById("calendar");
+    const calendar = new Calendar(calendarEl, {
+      initialView: "dayGridMonth",
+
+      selectable: true,
+      select: function (selected) {
+        console.log(selected.start);
+      },
+
+      events: [
+        {
+          title: "My Event",
+          start: "2024-03-01",
+        },
+      ],
+
+      eventMouseEnter: function (info) {
+        info.el.style.borderColor = "red"; //TODO cambiar Colores
+      },
+
+      eventMouseLeave: function (info) {
+        info.el.style.borderColor = "blue";
+      },
+
+      eventClick: function (info) {
+        info.el.style.borderColor = "red";
+        console.log("AAAAAA");
+      },
+
+      customButtons: {
+        myCustomButton: {
+          text: "custom!",
+          click: function () {
+            alert("clicked the custom button!");
+          },
+        },
+      },
+
+      headerToolbar: {
+        left: "prev,next today myCustomButton",
+        center: "title",
+        right: "dayGridMonth,timeGridWeek,timeGridDay",
+      },
+    });
+    calendar.render();
+  }
+
+  onMount(() => {
+    cal();
+  });
+</script>
+
+<div class="calendar-container" id="calendar"></div>
---
+++ src/Clock.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { onMount, onDestroy } from "svelte";
+  import { invoke } from "@tauri-apps/api/tauri";
+
+  let currentTime = "";
+
+  async function time() {
+    currentTime = await invoke("time", {});
+  }
+
+  const updateClock = () => {
+    time();
+  };
+
+  const interval = setInterval(updateClock, 1000);
+
+  onMount(() => {
+    updateClock();
+  });
+
+  onDestroy(() => {
+    clearInterval(interval);
+  });
+
+  function handleClick() {
+    document.getElementById("clock").classList.toggle("clock_expanded");
+    document.getElementById("calendar").classList.toggle("calendar_reduced");
+  }
+</script>
+
+<div id="clock" class="clock" on:click={handleClick}>
+  {currentTime}
+</div>
