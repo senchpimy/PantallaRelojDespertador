aad7baa49c24802ebc48e3cf1c7d6cc30ed9280c
senchpimy <senchpimy@gmail.com>
2024-03-13 20:48:23 -0600

    get events

--- v2/src-tauri/src/main.rs
+++ v2/src-tauri/src/main.rs
@@ -1,4 +1,5 @@
 use chrono::{DateTime, Local};
+use reqwest;
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -11,9 +12,19 @@ fn time() -> String {
     format!("{}", custom_format)
 }
 
+#[tauri::command]
+fn get_cal_data() -> String {
+    let json_response = r#"{"error": {"code": 500,"message": "Internal Server Error"}}"#;
+    let resp = reqwest::blocking::get("http://localhost:8080");
+    match resp {
+        Ok(val) => val.text().unwrap(),
+        Err(_) => String::from(json_response),
+    }
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, time])
+        .invoke_handler(tauri::generate_handler![greet, time, get_cal_data])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
