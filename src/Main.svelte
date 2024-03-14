d3dcea9c446fec4a7eb601e8bd6014d9f7ba5d29
senchpimy <senchpimy@gmail.com>
2024-03-14 00:26:28 -0600

    missing files

---
+++ src/Main.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Clock from "./Clock.svelte";
+  import Calendar from "./Calendar.svelte";
+  import Lista from "./Lista.svelte";
+</script>
+
+<div class="division">
+  <Calendar />
+  <div class="rigth_column">
+    <Clock />
+    <Lista />
+  </div>
+</div>
