86888696264927b26583fc7ecffe202ddbab4a81
senchpimy <senchpimy@gmail.com>
2024-03-18 14:19:35 -0600

    update List!

--- v2/src/utils.js
+++ v2/src/utils.js
@@ -4,7 +4,7 @@ let DATA = null;
 export async function get_cal_data(update) {
   if (DATA && update === false) {
     console.log("NO NUEVAS SOLICITUDES")
-    return DATA;
+    return DATA.events;
   }
   console.log("NUEVAS SOLICITUDES")
   let str = await invoke("get_cal_data", {});
