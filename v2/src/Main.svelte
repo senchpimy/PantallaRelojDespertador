eeceb55942391b2f7ca2982d024159744133efe8
senchpimy <senchpimy@gmail.com>
2024-03-18 17:59:04 -0600

    auto update

--- v2/src/Main.svelte
+++ v2/src/Main.svelte
@@ -1,23 +1,7 @@
 <script>
-  import { onMount, onDestroy } from "svelte";
   import Clock from "./Clock.svelte";
   import Calendar from "./Calendar.svelte";
   import Lista from "./Lista.svelte";
-  import { get_cal_data } from "./utils";
-
-  const updateData = () => {
-    get_cal_data(true);
-  };
-
-  const interval = setInterval(updateData, 10000);
-
-  onMount(() => {
-    get_cal_data(true);
-  });
-
-  onDestroy(() => {
-    clearInterval(interval);
-  });
 </script>
 
 <div class="division">
--- v2/src/utils.js
+++ v2/src/utils.js
@@ -1,12 +1,9 @@
 import { invoke } from "@tauri-apps/api/tauri";
 let DATA = null;
 
-export async function get_cal_data(update) {
-  if (DATA && update === false) {
-    console.log("NO NUEVAS SOLICITUDES")
-    return DATA.events;
-  }
-  console.log("NUEVAS SOLICITUDES")
+export const DATA_TIMEOUT = 1000 * 60 * 5;
+
+export async function get_cal_data() {
   let str = await invoke("get_cal_data", {});
   DATA = JSON.parse(str);
   return DATA.events
