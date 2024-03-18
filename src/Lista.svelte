6eb3d96009952745338a2c7470187f57b004a648
senchpimy <senchpimy@gmail.com>
2024-03-18 12:17:49 -0600

    colors

--- src/Lista.svelte
+++ src/Lista.svelte
@@ -1,18 +1,53 @@
 <script>
+  import { onMount } from "svelte";
   import { get_cal_data } from "./utils";
   var ElementsList = [];
   async function get_list() {
     ElementsList = await get_cal_data(false);
+    for (let i = ElementsList.length - 1; i >= 0; i--) {
+      ElementsList[i].color += "26"; //RGBA
+      if (ElementsList[i].type === "recurring") {
+        ElementsList.splice(i, 1);
+      } else {
+        if ("completed" in ElementsList[i] && ElementsList[i].completed) {
+          ElementsList[i].ready = true;
+        }
+        if (!("date" in ElementsList[i])) {
+          ElementsList[i].date = ElementsList[i].start.split("T")[0];
+        }
+      }
+    }
   }
   get_list();
+  function focus(node) {
+    node.addEventListener("click", function () {
+      var r = node.querySelector(".info");
+      r.classList.toggle("info-show");
+    });
+  }
+  function color(node) {
+    var r = node.querySelector("#point");
+    console.log(r);
+  }
 </script>
 
 <div id="lista" class="lista">
-  <ul>
+  <ul use:color>
     {#each ElementsList as element}
-      <li>
-        <h4>{element.title}</h4>
-        <p>Texto</p>
+      <li style="background-color:{element.color}" use:focus>
+        {#if "ready" in element}
+          <div class="lista_element cumplido">
+            {element.title}
+          </div>
+        {:else}
+          <div class="lista_element">
+            {element.title}
+          </div>
+        {/if}
+        {element.date}
+        <div class="info">
+          {element.info}
+        </div>
       </li>
     {/each}
   </ul>
