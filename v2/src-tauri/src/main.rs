173ac8386a6c8ed79530638ca9100f54a6ecc8fb
senchpimy <senchpimy@gmail.com>
2024-03-24 12:54:40 -0600

    pre

--- v2/src-tauri/src/main.rs
+++ v2/src-tauri/src/main.rs
@@ -27,10 +27,13 @@ impl AppCal {
                 file.read_to_string(&mut contents).unwrap();
                 serde_json::from_str(&contents).unwrap()
             }
-            Err(_) => Self {
-                port: String::from("8080"),
-                server: String::from("localhost"),
-            },
+            Err(a) => {
+                println!("ERROR: {}", a);
+                Self {
+                    port: String::from("8000"),
+                    server: String::from("127.0.0.1"),
+                }
+            }
         }
     }
 }
